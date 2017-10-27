20b13c609a7037be2cbd37491f20b73106e57655
Miguel <Miguel@192.168.1.38>
2017-10-27 14:48:19 -0500

    probando cambio

---
+++ src/TestPackageDAO/PruebaGit.java
@@ -0,0 +1,13 @@
+
+package TestPackageDAO;
+
+
+public class PruebaGit {
+    public static void main(String[] args) {
+        System.out.println("Esta es una prueba del mege git");
+        funcion();
+    }
+    public static void funcion(){
+        System.out.println("Probando funcion");
+    }
+}
